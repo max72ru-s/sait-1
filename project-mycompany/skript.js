584614b5e1d3f68ce195132038e18b8bb357191e
max72ru-s <64639600+max72ru-s@users.noreply.github.com>
2020-05-01 14:23:25 +0300

    sait beta

---
+++ project-mycompany/skript.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+  var x = document.getElementByld('myTopnaw-text');
+
+  if (x.className === "topnav-text") {
+    x.className += "responsite";
+  } else {
+    x.className = "topnav";
+  }
+}
